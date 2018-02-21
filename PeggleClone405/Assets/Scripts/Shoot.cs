e247a0f2e9084bfe5fbdfb5081d712647715b13c
Evan Snook <egs819@mail.usask.ca>
2018-02-21 01:36:24 -0600

    initial project setup

---
+++ PeggleClone405/Assets/Scripts/Shoot.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public float projectileSpeed; // Movement Speed of the projectile.
+    public GameObject projectilePrefab; // The Projectile that will be fired.
+    private GameObject clone; // The fired Projectile.
+    
+    private bool canFire; // This keeps track of if you can fire or not.
+    private Vector3 mousePosition;
+    private Quaternion angleToMouse;
+
+    void Start()
+    {
+        canFire = true;
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Fire1") > 0.1)
+        { // This get's the input for Fire1 and sends message to fire if pushed.
+            FireProjectileAtMouse();
+        }
+    }
+
+
+    // This Finds the Mouse and Fires a Projectile in the Mouses Direction.
+    private void FireProjectileAtMouse()
+    {
+        if (canFire)
+        {
+            canFire = false;
+
+            getMousePosition();
+            
+            clone = Instantiate(projectilePrefab, transform.position, angleToMouse) as GameObject;
+            clone.GetComponent<Rigidbody2D>().AddForce(clone.transform.up * -projectileSpeed); // Launch Projectile forward to Mouse.
+            
+        }
+
+    }
+
+    void getMousePosition()
+    {
+        // Get the Mouse Position on the screen
+        mousePosition = Input.mousePosition;
+
+        // subtract the cameras z axisfrom the mouse position to put the vecctor on the same plane as the game
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;
+
+        //change the cooridinate type from screen position of the computer to the world position within the game
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+        //get angle to the mouse from the gameObject which is instantiated pointing down
+        angleToMouse = Quaternion.FromToRotation(Vector3.down, mousePosition - transform.position);
+    }
+
+    public void resetFire()
+    {
+        canFire = true;
+    }
+}
---
+++ PeggleClone405/Assets/Scripts/followMouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followMouse : MonoBehaviour {
+
+    private Quaternion angleToMouse;
+    private Vector3 mousePosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getMousePosition();
+
+        //set the rotation to that angle to mouse
+        this.transform.rotation = angleToMouse;
+
+    }
+
+    void getMousePosition()
+    {
+        // Get the Mouse Position on the screen
+        mousePosition = Input.mousePosition;
+
+        // subtract the cameras z axisfrom the mouse position to put the vecctor on the same plane as the game
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;
+
+        //change the cooridinate type from screen position of the computer to the world position within the game
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //get angle to the mouse from the gameObject which is instantiated pointing down
+        angleToMouse = Quaternion.FromToRotation(Vector3.down, mousePosition - transform.position);
+    }
+
+}
