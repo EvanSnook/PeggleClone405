ab958afb1227e13bc58b6ded25efe9548fced9ef
Evan Snook <egs819@mail.usask.ca>
2018-03-31 02:03:41 -0600

    ui scaling improbements

---
+++ PeggleClone405/Assets/Scripts/slider_update_label.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slider_update_label : MonoBehaviour {
+
+    public GameObject input_slider;
+    private string original_text;
+	// Use this for initialization
+	void Start () {
+        original_text = this.gameObject.GetComponent<Text>().text;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.GetComponent<Text>().text = original_text + input_slider.GetComponent<Slider>().value.ToString();
+	}
+}
